0ec31a6ae04dd886f5858d3ef02100fa62f57c17
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 20:33:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,11 +23,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title
-  FROM series
-  INNER JOIN characters ON series_id = series.id 
-  WHERE species = 'human' 
-  GROUP BY title ORDER BY COUNT(*) DESC LIMIT 1"
+  "SELECT title FROM series
+  INNER JOIN characters ON series.author_id = characters.author_id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
